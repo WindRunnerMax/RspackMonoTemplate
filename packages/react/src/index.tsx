7f659c699b923d982d753db4f88c4f72d86016a6
WindrunnerMax <651525974@qq.com>
2024-04-21 21:04:12 +0800

    fix: html resources

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,7 +1,8 @@
+import { DateTime } from "mono-utils";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  return <h1>Hello, World! {new DateTime().toISOString()}</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
